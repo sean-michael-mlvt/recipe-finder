7f9f9816e9f3742f1c7316efb78373e2a5855a1e
Sean Malavet <smm65270@uga.edu>
2025-04-11 02:12:13 -0400

    Added Print message to SAVE PANTRY button

--- src/components/Pantry.tsx
+++ src/components/Pantry.tsx
@@ -12,33 +12,45 @@ interface PantryProps {
 
 function Pantry({pantryContents, onRemove}: PantryProps) {
 
+    // Function to be called when Save Pantry button is clicked
+    const handleSavePantry = () => {
+        console.log("PANTRY CONTENTS:");
+        console.log(pantryContents);
+    }
+
     return (
         <div className="w-full md:h-full h-[65vh] md:w-7/12 bg-white flex flex-col p-1 md:p-5 gap-5">
 
             {/* Pantry Items Container */}
             <div className="flex-grow bg-light-stegman border-2 border-solid border-black rounded p-5">
+
+                {/* Grid for item layout, map individual items to divs with x buttons */}
                 <div className="flex flex-wrap gap-3">
                     {pantryContents.map((ingredient) => (
                         <div
                         key={ingredient.id}
                         className="flex items-center gap-2 border-2 border-black rounded-[12px] px-4 py-2 text-black text-lg relative group bg-white"
                         >
-                        {ingredient.name}
-                        <button
-                            onClick={() => onRemove(ingredient.id)}
-                            className="text-gray-500 hover:text-black opacity-0 group-hover:opacity-100 transition-opacity duration-200"
-                            aria-label={`Remove ${ingredient.name}`}
-                        >
-                            ✕
-                        </button>
+                            {ingredient.name}
+
+                            {/* X button for removing item */}
+                            <button
+                                onClick={() => onRemove(ingredient.id)}
+                                className="text-gray-500 hover:text-black opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+                                aria-label={`Remove ${ingredient.name}`}
+                            >
+                                ✕
+                            </button>
                         </div>
                     ))}
                 </div>
             </div>
 
-            {/* Buttons */}
+            {/* Bottom Buttons */}
             <span className="flex justify-end gap-5">
-                <button className="cursor-pointer text-2xl text-white text-oswald px-6 py-3 bg-lake-herrick">
+                <button className="cursor-pointer text-2xl text-white text-oswald px-6 py-3 bg-lake-herrick" 
+                    onClick={handleSavePantry}
+                >
                     SAVE PANTRY
                 </button>
                 <button className="cursor-pointer text-2xl text-white text-oswald px-6 py-3 bg-bulldog-red">
